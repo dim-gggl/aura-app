140aff0a4e18c361839a0ef6d2a50edfcaf250dd
dim-gggl <dimitri.gaggioli@gmail.com>
2025-08-09 16:18:28 +0200

    templates: enregistrer 'notes.templatetags.markdown_extras' comme builtin pour accès global au filtre markdownify

--- aura_app/settings/base.py
+++ aura_app/settings/base.py
@@ -88,6 +88,9 @@ TEMPLATES = [
         "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
+            "builtins": [
+                "notes.templatetags.markdown_extras",
+            ],
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
